345972431c6f2c2c57a61dbe99012c72d1b742a4
Hani Benhabiles <kroosec@gmail.com>
2020-01-28 22:23:34 +0100

    Skeleton for Loading programs.

--- VM.go
+++ VM.go
@@ -1,5 +1,11 @@
 package lc3
 
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
 const (
 	MemorySize = int(1 << 16)
 )
@@ -36,4 +42,24 @@ func (v *VM) GetMemory(address uint16) uint16 {
 
 func (v *VM) GetRegister(reg Register) uint16 {
 	return v.registers[reg]
+}
+
+func (v *VM) Load(program io.Reader) error {
+	content, err := ioutil.ReadAll(program)
+	if err != nil {
+		return fmt.Errorf("Failed to load program: %v", err)
+	}
+
+	// Program must contain at least .ORIG, and must be multiple of 2, as each instruction is 16 bits long.
+	if len(content) < 2 || len(content)%2 != 0 {
+		return fmt.Errorf("Program size in bytes not multiple of two.")
+	}
+
+	// XXX: Get first 2 bytes: Set the PC.
+
+	for i := 2; i < len(content); i += 2 {
+		// XXX: For each two bytes, load as instruction (swap16?) into memory.
+	}
+
+	return nil
 }
--- VM_test.go
+++ VM_test.go
@@ -1,6 +1,8 @@
 package lc3_test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"lc3"
@@ -10,6 +12,35 @@ func TestVM(t *testing.T) {
 	t.Run("create a new VM and check zeroed memory", func(t *testing.T) {
 		vm := lc3.NewVM()
 		assertInitVM(t, vm)
+	})
+
+	t.Run("try to load erroneous programs", func(t *testing.T) {
+		vm := lc3.NewVM()
+		assertInitVM(t, vm)
+
+		testCases := []string{
+			"",
+			"\x00",
+			"\x01\x02\x03",
+		}
+
+		for i, test := range testCases {
+			t.Run(fmt.Sprintf("test case #%d", i), func(t *testing.T) {
+				program := strings.NewReader(test)
+				err := vm.Load(program)
+				assertIsError(t, err)
+			})
+		}
+	})
+
+	t.Run("load a simple program and check PC", func(t *testing.T) {
+		vm := lc3.NewVM()
+		assertInitVM(t, vm)
+
+		program := strings.NewReader("\x00\x30")
+		err := vm.Load(program)
+		assertError(t, err, nil)
+		assertRegister(t, vm, lc3.Register_PC, 0x3000)
 	})
 }
 
@@ -24,9 +55,31 @@ func assertInitVM(t *testing.T, vm *lc3.VM) {
 	}
 
 	for reg := lc3.Register_R0; reg < lc3.Register_COUNT; reg++ {
-		value := vm.GetRegister(reg)
-		if value != 0 {
-			t.Fatalf("expected register %d's value to be zero, got %d", reg, value)
-		}
+		assertRegister(t, vm, reg, 0)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Fatalf("expected error %+v, got %+v", want, got)
+	}
+}
+
+func assertIsError(t *testing.T, err error) {
+	t.Helper()
+
+	if err == nil {
+		t.Fatalf("expected error, got none")
+	}
+}
+
+func assertRegister(t *testing.T, vm *lc3.VM, reg lc3.Register, want uint16) {
+	t.Helper()
+
+	got := vm.GetRegister(reg)
+	if got != want {
+		t.Fatalf("expected register %d's value to be 0x%x, got 0x%x", reg, want, got)
 	}
 }
